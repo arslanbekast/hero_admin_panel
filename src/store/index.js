51530c95620f0c48734e4dc093ce675bd70fb4c4
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-07-22 20:57:33 +0300

    Commit

--- src/store/index.js
+++ src/store/index.js
@@ -13,20 +13,20 @@ const stringMiddleware = (store) => (next) => (action) => {
     return next(action);
 }
 
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
+// const enhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
 
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch({
-                type: action
-            });
-        }
-        return oldDispatch(action);
-    }
-    return store;
-}
+//     const oldDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispatch({
+//                 type: action
+//             });
+//         }
+//         return oldDispatch(action);
+//     }
+//     return store;
+// }
 
 const store = createStore(
                     combineReducers({heroes, filters}), 
