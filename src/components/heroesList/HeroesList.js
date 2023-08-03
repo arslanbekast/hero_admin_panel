fac430ef2d1a91d4bbc9a25ef21c8c3a440d67e0
Arslanbek Astemirov <s-o-f-t@mail.ru>
2023-08-03 11:52:02 +0300

    Used the createAsyncThunk() function from the Redux Toolkit to create actions

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -2,10 +2,9 @@ import {useHttp} from '../../hooks/http.hook';
 import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
-import { createSelector } from 'reselect';
+import { createSelector } from '@reduxjs/toolkit';
 
-import { fetchHeroes } from '../../actions';
-import { heroDeleted } from './heroesSlice'
+import { heroDeleted, fetchHeroes } from './heroesSlice'
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
@@ -44,7 +43,7 @@ const HeroesList = () => {
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(fetchHeroes(request));
+        dispatch(fetchHeroes());
 
         // eslint-disable-next-line
     }, []);
